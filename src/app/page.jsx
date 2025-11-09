af8b5fb6958919fce3a5628856555cff0bb54fca
khussalpradhan <pradhankhu@gmail.com>
2025-11-09 01:39:07 -0600

    revert: restore homepage image size to previous layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,16 +15,16 @@ const Homepage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-2/3 flex items-center justify-center">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center">
           {/* Rounded white panel + shadow to separate the photo from the page background */}
-          <div className="relative w-full h-96 md:h-[28rem] lg:h-[42rem] lg:w-11/12 rounded-2xl overflow-hidden flex items-center justify-center">
+          <div className="relative w-full h-72 lg:h-full lg:w-11/12 rounded-2xl overflow-hidden flex items-center justify-center">
             {/* Use unoptimized so Next's custom loader is bypassed and the local public file is served */}
-            {/* Use object-cover so the image fills more of the panel and appears larger */}
-            <Image src="/file_3_nobg.png" alt="Khussal Pradhan" fill className="object-cover" unoptimized />
+            {/* mix-blend-multiply applied so image edges blend with panel background. Other options: mix-blend-screen, mix-blend-overlay, mix-blend-darken, mix-blend-lighten */}
+            <Image src="/file_3_nobg.png" alt="Khussal Pradhan" fill className="object-contain" unoptimized />
           </div>
         </div>
         {/* TEXT CONTAINER */}
-  <div className="h-1/2 lg:h-full lg:w-1/3 flex flex-col gap-8 items-center justify-center">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
             Hi! I'm Khussal Pradhan!
