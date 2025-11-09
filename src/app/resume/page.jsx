69a9c48b4b8957f4dc9a5fa668033cfdde5260fa
khussalpradhan <pradhankhu@gmail.com>
2025-11-09 01:59:53 -0600

    fixed ui

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -9,7 +9,7 @@ const ResumePage = () => {
         Discover my professional journey by accessing my resume
       </p> */}
       <a 
-        href="https://drive.google.com/file/d/1l9bjQDNZZsJNNibMLU0g8Wv8z0w26tMQ/view?usp=sharing" 
+        href="https://drive.google.com/file/d/19B_z7u4KDPTvCBtPPNwvUhmHKMgOtOyR/view?usp=sharing" 
         target="_blank" 
         rel="noopener noreferrer" 
         style={styles.link}
